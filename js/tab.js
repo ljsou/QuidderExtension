71facb882901e1f68a6d5548c196e6f03dc4dcdc
Javier Suarez <luisjavier.suarezmeza@gmail.com>
2013-11-04 01:39:13 -0500

    implementing quidder extension navigation flow based on user authentication

--- js/tab.js
+++ js/tab.js
@@ -3,11 +3,29 @@
  * and open the template in the editor.
  */
 
-console.log("entró");
+console.log("entró: " + localStorage.getItem('pin') + ". .");
+
+if(localStorage.getItem('pin') == null){
+    console.log("pin is null");
+    chrome.browserAction.setPopup({
+        popup:"QuidderExSignUp.html"
+    });
+}if(localStorage.getItem('pin') == ''){
+    console.log("pin is '.'");
+    chrome.browserAction.setPopup({
+        popup:"QuidderExSignUp.html"
+    });
+}if(localStorage.getItem('pin') != '') {
+    console.log("pin is not null");
+    chrome.browserAction.setPopup({
+        popup:"TaggingBox.html"
+    });
+}
 
 chrome.tabs.query({active: true, windowId: chrome.windows.WINDOW_ID_CURRENT}, function(tabs) {
+    console.log("tab query");
     var url = tabs[0].url;
     console.log("url: " + url);
     document.getElementById('url').innerHTML = url;
     $('#url').val(url);
-});
+})
